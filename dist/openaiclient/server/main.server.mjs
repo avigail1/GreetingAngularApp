cddc0da6551f742d1a8c93346d15595d784dd0c6
avigail1 <149592270+avigail1@users.noreply.github.com>
2024-08-01 18:20:35 +0300

    add css

--- dist/openaiclient/server/main.server.mjs
+++ dist/openaiclient/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6ROJBPTF.mjs";import"./chunk-LLSQ7PDZ.mjs";import"./chunk-S6DUPUCS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-6O6PUEEM.mjs";import"./chunk-LLSQ7PDZ.mjs";import"./chunk-S6DUPUCS.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    OPEN_AI_KEY: 'sk-proj-kijFFSPa65xOtZSvbdweT3BlbkFJAUm68gKUKX49FMcyju43'
+    OPEN_AI_KEY: 'sk-None-4L50rQV6KR8rk89udrYdT3BlbkFJwwZZTI0U5nPfOnCoQKq9'
   };
   
